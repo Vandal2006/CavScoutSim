90b8df38ba973425f95a14900f18042c807c502f
Vandal2006 <henryjgilson@lewisu.edu>
2022-06-26 11:40:47 -0500

    added model window.

--- client/test.js
+++ client/test.js
@@ -1,4 +1,9 @@
 console.log("Starting Cav Scout Simulator...");
+
+
+
+
+//function for scoring test
 function calculateCompatibility() {
 	console.log("calculateCompatibility()");
 	const DESIRED_RESPONSE = [
@@ -50,4 +55,33 @@ function calculateCompatibility() {
         else {document.getElementById("explanation").innerHTML = "YOU HAVE FAILED!"
 
     }
+}
+
+
+
+//modal window
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
