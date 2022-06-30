09f64c044a063c9558e549d14d8f258668e9480c
Vandal2006 <henryjgilson@lewisu.edu>
2022-06-29 19:21:39 -0500

    added JSON js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response, application } = require("express");
 const express = require("express");
 const app = express();
 
@@ -11,4 +12,22 @@ app.get('/test', function(request, response) {
 
 app.listen(port, function() {
 	console.log("Server is running at http://localhost:3000/")
+})
+
+
+
+//JSON of images
+var Img ={
+        "abramsIMG":"https://upload.wikimedia.org/wikipedia/commons/e/e8/M1A1_Trophy_Technology_Demonstrator.jpg",
+        "t72IMG":"https://upload.wikimedia.org/wikipedia/commons/f/fa/T-72_Ajeya1.jpg",
+		"bradlyIMG":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Decisive_Action_Rotation_13-04_130218-A-ML570-001.jpg/1280px-Decisive_Action_Rotation_13-04_130218-A-ML570-001.jpg",
+		"bmpIMG":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Army2016demo-011.jpg/1920px-Army2016demo-011.jpg"
+}
+
+
+app.get('/Img', (request, response) =>{
+	console.log('Calling "Img" on the node.js server')
+	response.type('application.json')
+	response.send(JSON.stringify(Img, null, 4))
+
 })
